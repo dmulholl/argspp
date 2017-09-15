826861123c1d23a8d3be13269f0ae622d7a8ee99
Darren Mulholland <darren@mulholland.xyz>
2017-09-15 12:22:35 +0100

    Housekeeping

--- src/optional.h
+++ src/optional.h
@@ -3,7 +3,7 @@
 //
 // Author: Darren Mulholland <darren@mulholland.xyz>
 // License: Public Domain
-// Version: 0.2.0
+// Version: 0.3.0
 // -----------------------------------------------------------------------------
 
 #pragma once
